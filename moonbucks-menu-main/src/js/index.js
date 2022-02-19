1a965dc1ac300b9163e4b77c7d9f8dd8594df9ba
kwonjs1996 <kwonjs1996@gmail.com>
2022-02-19 15:05:17 +0900

    메뉴 추가시 클릭 엔터 이벤트 합치는 과정에서 해결본 괄호 쓰기 중요 실행 순서

--- moonbucks-menu-main/src/js/index.js
+++ moonbucks-menu-main/src/js/index.js
@@ -16,35 +16,59 @@ function App(){
         e.preventDefault();
     });
 
-
-    //메뉴의 이름을 입력받는건
-   $("#espresso-menu-name").addEventListener("keypress", (e) =>{
-      if(e.key==="Enter"){
-          const espressoMenuName = $("#espresso-menu-name").value;
-          const menuItemTemplate = (espressoMenuName)=> {
-          return`
-            <li class="menu-list-item d-flex items-center py-2">
-            <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
-            <button
-            type="button"
-            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-            >
-            수정
-            </button>
-            <button
-            type="button"
-            class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-            >
-            삭제
-            </button>
-        </li>`;
-      };
+    const addMenuname = (e) => {
+      if ($("#espresso-menu-name").value === ""){
+        alert("값을 입력해주세요.");
+        return;
+      }
+     
+        const espressoMenuName = $("#espresso-menu-name").value;
+        const menuItemTemplate = (espressoMenuName)=> {
+        return`
+          <li class="menu-list-item d-flex items-center py-2">
+          <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+          <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+          >
+          수정
+          </button>
+          <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+          >
+          삭제
+          </button>
+      </li>`;
+        }
       $("#espresso-menu-list").insertAdjacentHTML(
         "beforeend", menuItemTemplate(espressoMenuName)
         );
+        const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+        $(".menu-count").innerText = `총 ${menuCount} 개`;
+        $("#espresso-menu-name").value = "";
       }
+    
+
+
+
+    //메뉴의 이름을 입력받는건
+   $("#espresso-menu-name").addEventListener("keypress", (e) =>{
+     if(e.key !== "Enter"){
+       return
+     }
+     
+
+       addMenuname(e);
     });
-}
+
+    $("#espresso-menu-submit-button").addEventListener("click", (e)=>{
+      addMenuname(e);
+    });
+     
+    
+    }
+
 
 App();
 
