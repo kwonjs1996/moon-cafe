c318d6e370835f566005c1fe65deeea16f5be0e9
kwonjs1996 <kwonjs1996@gmail.com>
2022-02-20 18:08:54 +0900

    위임을 이용해서 프롬프트로 수정 삭제 기능 구현

--- moonbucks-menu-main/src/js/index.js
+++ moonbucks-menu-main/src/js/index.js
@@ -7,6 +7,14 @@
 // - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
 // - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
+// TODO 메뉴 수정
+// - [ ] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴수정하는 모달창이 뜬다.
+// - [ ] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+        
+
+//TODO 메뉴 삭제
+    // - [ ] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
+    // - [ ] 확인 버튼을 클릭하면 메뉴가 삭제된다.
 
 const $ = (selector) => document.querySelector(selector);
 
@@ -16,7 +24,7 @@ function App(){
         e.preventDefault();
     });
 
-    const addMenuname = (e) => {
+    const addMenuname = () => {
       if ($("#espresso-menu-name").value === ""){
         alert("값을 입력해주세요.");
         return;
@@ -44,10 +52,46 @@ function App(){
       $("#espresso-menu-list").insertAdjacentHTML(
         "beforeend", menuItemTemplate(espressoMenuName)
         );
-        const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-        $(".menu-count").innerText = `총 ${menuCount} 개`;
+        updateMenuCount();
         $("#espresso-menu-name").value = "";
       }
+
+
+    
+    const updateMenuCount = () => {
+      const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+        $(".menu-count").innerText = `총 ${menuCount} 개`
+    };
+
+    $("#espresso-menu-list").addEventListener("click", (e) => {
+      //<li>태그로 추가되는 것들에게 이Event를 넣기 위해서 상위태그에 '위임'하는
+      //방식으로 적용한다
+      if (e.target.classList.contains("menu-edit-button")){
+        const $menuName = e.target.closest("li").querySelector(".menu-name")
+        const updatedMenuNme = prompt(
+          "메뉴명을 수정하세요",
+           $menuName.innerText
+           //$menuName 만 입력했을 경우 [object HTMLSpanElement] 값이 나온다
+           //그래서 innerText를 사용한다 정도로 이해
+           );
+        $menuName.innerText = updatedMenuNme;
+      }
+
+      if(e.target.classList.contains("menu-remove-button")){
+        if (confirm("정말 삭제하시겠습니까?")){
+            e.target.closest("li").remove();
+            // qurryselector 를 사용하지 않는 이유는 li 태그 전부를
+            //삭제 해야 하기 때문
+            updateMenuCount();
+
+        }
+     
+      }
+      
+    });
+
+
+
     
 
 
@@ -65,6 +109,10 @@ function App(){
     $("#espresso-menu-submit-button").addEventListener("click", (e)=>{
       addMenuname(e);
     });
+
+
+    
+    
      
     
     }
@@ -83,11 +131,7 @@ App();
 
 
 
-// TODO 메뉴 수정
-// - [ ] 메뉴의 수정 버튼클릭 이벤트를 받고, 메뉴수정하는 모달창이 뜬다.
-// - [ ] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
 
-//TODO 메뉴 삭제
-// - [ ] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
-// - [ ] 확인 버튼을 클릭하면 메뉴가 삭제된다.
+
+
 
