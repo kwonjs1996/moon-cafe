261230065b5feae1955f7f26ae9de47c7fd1d0b6
kwonjs1996 <kwonjs1996@gmail.com>
2022-02-22 16:44:47 +0900

    javascrit의 this undefined 해결 방법

--- moonbucks-menu-main/src/js/index.js
+++ moonbucks-menu-main/src/js/index.js
@@ -1,3 +1,9 @@
+// 오늘 얻은 인사이트
+// 이벤트 위임을 어떻게 할 수 있는지 알앗따
+// 요구사항을 전략적으로 접근, 세세하게 나누는게 중요하다
+// DOM 요소를 $를 써서 변수 처럼 사용 할 수 있다.
+
+
 // 요구사항 구현을 위한 전략
 
 //TODO 메뉴 추가
@@ -16,9 +22,22 @@
     // - [ ] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
     // - [ ] 확인 버튼을 클릭하면 메뉴가 삭제된다.
 
+
 const $ = (selector) => document.querySelector(selector);
 
+const store = {
+  setLocalStorage(menu) {
+      localStorage.setItem("menu",JSON.stringify(menu));
+  },
+  getLocalStorage() {
+      localStorage.getItem("menu");
+  },
+}
+
 function App(){
+  // 상태는 변하는 데이터, 이 앱에서 변하는 것이 무엇인가 - 메뉴명
+  this.menu = [];
+
     //form태그가 자동으로 전송되는걸 막아준다
     $("#espresso-menu-form").addEventListener("submit", (e) => {
         e.preventDefault();
@@ -29,12 +48,15 @@ function App(){
         alert("값을 입력해주세요.");
         return;
       }
-     
+      
         const espressoMenuName = $("#espresso-menu-name").value;
-        const menuItemTemplate = (espressoMenuName)=> {
-        return`
+      
+        this.menu.push({ name: espressoMenuName });
+        store.setLocalStorage(this.menu);
+        const template = this.menu.map((item) => {
+          return`
           <li class="menu-list-item d-flex items-center py-2">
-          <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+          <span class="w-100 pl-2 menu-name">${item.name}</span>
           <button
           type="button"
           class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
@@ -48,10 +70,10 @@ function App(){
           삭제
           </button>
       </li>`;
-        }
-      $("#espresso-menu-list").insertAdjacentHTML(
-        "beforeend", menuItemTemplate(espressoMenuName)
-        );
+        })
+        .join("")
+        
+      $("#espresso-menu-list").innerHTML = template;
         updateMenuCount();
         $("#espresso-menu-name").value = "";
       }
@@ -72,6 +94,14 @@ function App(){
       const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
         $(".menu-count").innerText = `총 ${menuCount} 개`
     };
+    const removeMenuName = (e) => {
+      if (confirm("정말 삭제하시겠습니까?")){
+        e.target.closest("li").remove();
+        // qurryselector 를 사용하지 않는 이유는 li 태그 전부를
+        //삭제 해야 하기 때문
+        updateMenuCount();
+      }
+    }
 
     $("#espresso-menu-list").addEventListener("click", (e) => {
       //<li>태그로 추가되는 것들에게 이Event를 넣기 위해서 상위태그에 '위임'하는
@@ -81,23 +111,11 @@ function App(){
       }
 
       if(e.target.classList.contains("menu-remove-button")){
-        if (confirm("정말 삭제하시겠습니까?")){
-            e.target.closest("li").remove();
-            // qurryselector 를 사용하지 않는 이유는 li 태그 전부를
-            //삭제 해야 하기 때문
-            updateMenuCount();
-
-        }
+        removeMenuName(e);
      
       }
       
     });
-
-
-
-    
-
-
 
     //메뉴의 이름을 입력받는건
    $("#espresso-menu-name").addEventListener("keypress", (e) =>{
@@ -106,24 +124,35 @@ function App(){
      }
      
 
-       addMenuname(e);
+       addMenuname();
     });
 
-    $("#espresso-menu-submit-button").addEventListener("click", (e)=>{
-      addMenuname(e);
-    });
-
-
-    
-    
-     
-    
+    $("#espresso-menu-submit-button").addEventListener("click", addMenuname);
     }
 
 
-App();
+    const app = new App();
+    //App();
 
+//TODO localstorage Read & Write
+// [ ] [localStorage]에 데이터를 저장한다. 
+// [ ]  [localstorage]에 있는 데이터를 읽어온다.
 
+//TODO 카테고리별 메뉴판 관리
+// [ ] 에스프레소 메뉴판 관리
+// [ ] 프라푸치노 메뉴판 관리
+// [ ] 블렌디드 메뉴판 관리
+// [ ] 디저트 메뉴판 관리
+
+//TODO 페이지 접근시 최초 데이터 Reead & Rendering
+// [ ] 페이지에 최초로 로딩 할 때 localStorage에 에스프레소 메뉴를 읽어온다
+// [ ] 에스프레소 메뉴를 페이지에 그려준다
+// TOODO 품절 상태 관리
+// [ ] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out calss를
+// [ ] 추가하여 상태를 변경한다.
+// [ ] 품절 버튼을 추가한다.
+// [ ] 품절 버튼을 클릭하면 localStorage 에 상태값이 저장된다.
+// [ ] 클릭 이벤트에서 가장 가까운 li태그의 class속성 값에 sold-out을 추가한다.
 
 
 
