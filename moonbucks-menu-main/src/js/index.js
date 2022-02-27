fbed9e0017aee7a7b8880ae40ef290c2f66b5744
kwonjs1996 <kwonjs1996@gmail.com>
2022-02-27 15:21:06 +0900

    데이터 철자 오류 prappucino => frappucino 스토리시 비우고 다시실행 해결

--- moonbucks-menu-main/src/js/index.js
+++ moonbucks-menu-main/src/js/index.js
@@ -38,7 +38,7 @@ function App(){
   // 상태는 변하는 데이터, 이 앱에서 변하는 것이 무엇인가 - 메뉴명
   this.menu = {
     espresso : [],
-    prappuccino : [],
+    frappuccino : [],
     blended : [],
     teavana : [],
     desert : []
